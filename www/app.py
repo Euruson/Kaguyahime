fa76cd1d157baf30a13173dfc56caeb3d10aaf6c
Euruson <euruson@outlook.com>
2019-05-09 14:36:26 +0800

    Add the log module and its configuration file

--- www/app.py
+++ www/app.py
@@ -1,7 +1,17 @@
-import logging
-logging.basicConfig(level=logging.INFO)
+import logging.config
 import asyncio
+import os
+import json
 from aiohttp import web
+
+def setup_logging(default_path = "conf/logging.json",default_level = logging.INFO):
+    path = default_path
+    if os.path.exists(path):
+        with open(path,"r") as f:
+            config = json.load(f)
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level = default_level)
 
 
 ## 定义服务器响应请求的的返回为 "Awesome Website"
@@ -17,4 +27,5 @@ def init():
 
 
 if __name__ == "__main__":
+    setup_logging()
     init()
